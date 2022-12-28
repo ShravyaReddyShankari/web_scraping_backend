5ff8fd3e52e05d6cee170d45742f0b8aa714b905
ShravyaReddyShankari <33656484+ShravyaReddyShankari@users.noreply.github.com>
2022-12-28 17:24:22 -0600

    Update settings.py

--- web_scraping_project/settings.py
+++ web_scraping_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(3)3j%bcw#%yzw^n)er=5_n=f2xuy4y4jbh3usl$^e5wlk!1yk'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
